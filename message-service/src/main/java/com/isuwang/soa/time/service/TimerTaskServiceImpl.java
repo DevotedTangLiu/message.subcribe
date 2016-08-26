f597aa065bf4bb08888afed670d97a1f66167b93
tangliu <tangliu@mail2.sysu.edu.cn>
2016-08-26 11:11:45 +0800

    简化

--- message-service/src/main/java/com/isuwang/soa/time/service/TimerTaskServiceImpl.java
+++ message-service/src/main/java/com/isuwang/soa/time/service/TimerTaskServiceImpl.java
@@ -3,7 +3,6 @@ package com.isuwang.soa.time.service;
 import com.isuwang.dapeng.core.SoaException;
 import com.isuwang.dapeng.core.timer.ScheduledTask;
 import com.isuwang.dapeng.core.timer.ScheduledTaskCron;
-import com.isuwang.dapeng.hello.HelloServiceClient;
 
 /**
  * Created by tangliu on 2016/8/17.
@@ -14,8 +13,7 @@ public class TimerTaskServiceImpl implements TimeTaskService {
     @Override
     @ScheduledTaskCron(cron = "0 0/1 * * * ?")
     public void timerTask1() throws SoaException {
-        HelloServiceClient client = new HelloServiceClient();
-        client.sayHello("tang");
+        System.out.println("timerTask1 is being executed");
     }
 
     @Override
